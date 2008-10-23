44e6cc660657ff4424f9fc516d5f84a5199c1c0e
mark <mark@MSuppes.local>
2008-10-22 22:04:54 -0400

    adding cylindrical knowckout

--- mged_controller.rb
+++ mged_controller.rb
@@ -43,8 +43,18 @@ icosahedron = Matrix[
 	[-phi, 0, -1]
 ]
 
-`rm /Users/mark/Documents/edge/lib/test3.g`
+DB = "test3.g"
+torus = 0.125
+torus_negative = 0.90 * torus
+torus_ring_size = 1
+
+`rm #{DB}`
+# `/usr/brlcad/bin/mged -f -c  #{DB} 'kill -f * '`
+
 icosahedron.row_vectors().each_with_index do |v,index|
-	`/usr/brlcad/bin/mged -f -c  test3.g 'in torus#{index}.s tor #{v[0]} #{v[1]} #{v[2]} #{v[0]} #{v[1]} #{v[2]} 1.0 0.125'`
+	`/usr/brlcad/bin/mged -f -c  #{DB} 'in torus#{index}.t tor #{v[0]} #{v[1]} #{v[2]} #{v[0]} #{v[1]} #{v[2]} #{torus_ring_size} #{torus}'`
+	`/usr/brlcad/bin/mged -f -c  #{DB} 'in torus_negative#{index}.t tor #{v[0]} #{v[1]} #{v[2]} #{v[0]} #{v[1]} #{v[2]} #{torus_ring_size} #{torus_negative}'`
+	`/usr/brlcad/bin/mged -f -c  #{DB} 'in cylinder_knockout#{index} rcc #{v[0]} #{v[1]} #{v[2]} #{v[0]} #{v[1]} #{v[2]} #{torus_ring_size* 1.2}.'`
+	`/usr/brlcad/bin/mged -f -c  #{DB} 'r torus_shell.r u torus#{index}.t - torus_negative#{index}.t - cylinder_knockout#{index} '`
 end
 # `/usr/brlcad/bin/mged -f /Users/mark/Documents/edge/lib/test3.g`
