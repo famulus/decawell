39c80231b866fcd7d958cc96782f64b9ee12035a
mark <mark@MSuppes.local>
2008-10-26 12:43:34 -0400

    building function to calc intersection

--- mged_controller.rb
+++ mged_controller.rb
@@ -47,6 +47,19 @@ def cross_product(v1,v2)
 	Vector[(v1[1]*v2[2] - v1[2]*v2[1]),(v1[2]*v2[0] - v1[0]*v2[2]), (v1[0]*v2[1] - v1[1]*v2[0])]
 end
 
+def average(v1,v2)
+	Vector[(v1[0]+v2[0]/2),(v1[1]+v2[1]/2),(v1[2]+v2[2]/2)]
+end
+
+def intersection(a,b,c,d) # find intersection of line segment ab and cd in 3D
+	    det=Matrix[a[0],a[1],a[2],b[0],b[1],b[2],c[0],c[1],c[2]].det
+     detx=Matrix[d[0],d[1],d[2],b[0],b[1],b[2],c[0],c[1],c[2]].det
+     dety=Matrix[a[0],a[1],a[2],d[0],d[1],d[2],c[0],c[1],c[2]].det
+     detz=Matrix[a[0],a[1],a[2],b[0],b[1],b[2],d[0],d[1],d[2]].det
+               
+	
+	
+end
 
 
 DB = "test3.g"
@@ -58,7 +71,7 @@ torus_ring_size = 1
 `rm #{DB}`
 
 icosahedron.row_vectors().each_with_index do |v,index|
-	# `#{mged} 'in torus#{index} tor #{v[0]} #{v[1]} #{v[2]} #{v[0]} #{v[1]} #{v[2]} #{torus_ring_size} #{torus}'`
+	`#{mged} 'in torus#{index} tor #{v[0]} #{v[1]} #{v[2]} #{v[0]} #{v[1]} #{v[2]} #{torus_ring_size} #{torus}'`
 	# `#{mged} 'in torus_negative#{index} tor #{v[0]} #{v[1]} #{v[2]} #{v[0]} #{v[1]} #{v[2]} #{torus_ring_size} #{torus_negative}'`
 	# `#{mged} 'in cylinder_knockout#{index} rcc #{v[0]} #{v[1]} #{v[2]} #{v[0]} #{v[1]} #{v[2]} #{torus_ring_size* 1.2}.'`
 	# `#{mged} 'r torus_shell#{index} u torus#{index} - torus_negative#{index} - cylinder_knockout#{index}'`
@@ -76,11 +89,15 @@ icosahedron.row_vectors().each_with_index do |v,index|
 			# `#{mged} 'in line#{index}_#{index_in} rcc #{v[0]} #{v[1]} #{v[2]} #{h[0]} #{h[1]} #{h[2]} 0.1'` 
 			puts "cross_product#{ a =cross_product(h,v)}"
 			puts "cross_productb#{ b =cross_product(v,a)}"
-			
+			puts "cross_productc#{ c =cross_product(h,vin)}"
+			puts "cross_productc#{ d =cross_product(vin,c)}"
+			b =b * 0.25
+			d =d * 0.25
 			# `#{mged} 'in dot#{index}_#{index_in} sph #{b[0]} #{b[1]} #{b[2]} 0.1'` 
 			# `#{mged} 'in line2#{index}_#{index_in} rcc #{v[0]} #{v[1]} #{v[2]} #{a[0]} #{a[1]} #{a[2]} 0.1'` 
-			`#{mged} 'in line3#{index}_#{index_in} rcc #{v[0]} #{v[1]} #{v[2]} #{b[0]} #{b[1]} #{b[2]} 0.05'` 
-
+			`#{mged} 'in line3#{index}_#{index_in} rcc #{v[0]} #{v[1]} #{v[2]} #{b[0]} #{b[1]} #{b[2]} 0.01'` 
+			e = average(b,d)
+			# `#{mged} 'in dot#{index}_#{index_in} sph #{e[0]} #{e[1]} #{e[2]} 0.1'` 
 		end
 
 	end
