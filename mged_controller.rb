780ed223a30ff5f5ea858704c223ad77bca5e196
mark <mark@MSuppes.local>
2008-10-31 22:48:04 -0400

    added breaks for error checking

--- mged_controller.rb
+++ mged_controller.rb
@@ -24,28 +24,31 @@ icosahedron.row_vectors().each_with_index do |v,index|
 		h = vin - v #vector connecting the point pairs
 		i = v - vin #the inverse of h
 		if h.r == 2 && i.r ==2 # only render the outer most vertices
+			puts "index:#{index},index_in:#{index_in}"
 			# `#{mged} 'in line#{index}_#{index_in} rcc #{v[0]} #{v[1]} #{v[2]} #{h[0]} #{h[1]} #{h[2]} 0.1'` #the icosohedron
 			a =cross_product(h,v)
 			b =cross_product(v,a) 
-			
+
 			c =cross_product(i,vin)
 			d =cross_product(vin,c)
-			
-			# b =b * 0.25 #shorten the vectors
-			# d =d * 0.25
+
+			b =b * 0.25 #shorten the vectors
+			d =d * 0.25
 			# `#{mged} 'in dot#{index}_#{index_in} sph #{b[0]} #{b[1]} #{b[2]} 0.1'` 
 			# `#{mged} 'in line2#{index}_#{index_in} rcc #{v[0]} #{v[1]} #{v[2]} #{a[0]} #{a[1]} #{a[2]} 0.1'` 
-			# `#{mged} 'in line_right_#{index}_#{index_in} rcc #{v[0]} #{v[1]} #{v[2]} #{b[0]} #{b[1]} #{b[2]} 0.01'` 
-			# `#{mged} 'in line_left_#{index}_#{index_in} rcc #{vin[0]} #{vin[1]} #{vin[2]} #{d[0]} #{d[1]} #{d[2]} 0.01'` 
-			e = intersection(v,b+v,vin,d+vin)
-			puts "OKOK:#{e}"
-			`#{mged} 'in dot#{index}_#{index_in} sph #{e[0]} #{e[1]} #{e[2]} 0.1'`  rescue nil
-			# `#{mged} 'in a_#{index}_#{index_in} sph #{v[0]} #{v[1]} #{v[2]} 0.1'`  rescue nil
-			# `#{mged} 'in b_#{index}_#{index_in} sph #{(b+v)[0]} #{(b+v)[1]} #{(b+v)[2]} 0.1'`  rescue nil
-			# `#{mged} 'in c_#{index}_#{index_in} sph #{vin[0]} #{vin[1]} #{vin[2]} 0.1'`  rescue nil
-			# `#{mged} 'in d_#{index}_#{index_in} sph #{(d+vin)[0]} #{(d+vin)[1]} #{(d+vin)[2]} 0.1'`  rescue nil
+			`#{mged} 'in line_right_#{index}_#{index_in} rcc #{v[0]} #{v[1]} #{v[2]} #{b[0]} #{b[1]} #{b[2]} 0.01'` 
+			`#{mged} 'in line_left_#{index}_#{index_in} rcc #{vin[0]} #{vin[1]} #{vin[2]} #{d[0]} #{d[1]} #{d[2]} 0.01'` 
+			
+			e = intersection(v,v+b,vin,vin+d)
+			# puts "OKOK:#{e}"
+			# `#{mged} 'in dot#{index}_#{index_in} sph #{e[0]} #{e[1]} #{e[2]} 0.1'`  rescue nil
+			`#{mged} 'in a_#{index}_#{index_in} sph #{v[0]} #{v[1]} #{v[2]} 0.1'`  rescue nil
+			`#{mged} 'in b_#{index}_#{index_in} sph #{(b+v)[0]} #{(b+v)[1]} #{(b+v)[2]} 0.1'`  rescue nil
+			`#{mged} 'in c_#{index}_#{index_in} sph #{vin[0]} #{vin[1]} #{vin[2]} 0.1'`  rescue nil
+			`#{mged} 'in d_#{index}_#{index_in} sph #{(d+vin)[0]} #{(d+vin)[1]} #{(d+vin)[2]} 0.1'`  rescue nil
+			break
 		end                                                            
-
+		break
 	end
 end
 
