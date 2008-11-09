25f96720359af6e54d8198cc782808b9f7093487
mark <mark@criticalmetrics.com>
2008-11-08 19:35:33 -0500

    almost got it

--- mged_controller.rb
+++ mged_controller.rb
@@ -1,4 +1,3 @@
-require 'matrix'
 require 'geometry'
 require 'set'
 require 'facets'
@@ -12,64 +11,62 @@ torus_ring_size = 1
 
 `rm ./#{DB}`
 
-icosahedron.row_vectors().each_with_index do |v,index|
-	# `#{mged} 'in torus#{index} tor #{v[0]} #{v[1]} #{v[2]} #{v[0]} #{v[1]} #{v[2]} #{torus_ring_size} #{torus}'`
+Dodecahedron.icosahedron.each_with_index do |v,index|
+	`#{mged} 'in torus#{index} tor #{v[0]} #{v[1]} #{v[2]} #{v[0]} #{v[1]} #{v[2]} #{torus_ring_size} #{torus}'`
 	# `#{mged} 'in torus_negative#{index} tor #{v[0]} #{v[1]} #{v[2]} #{v[0]} #{v[1]} #{v[2]} #{torus_ring_size} #{torus_negative}'`
 	# `#{mged} 'in cylinder_knockout#{index} rcc #{v[0]} #{v[1]} #{v[2]} #{v[0]} #{v[1]} #{v[2]} #{torus_ring_size* 1.2}.'`
 	# `#{mged} 'r torus_shell#{index} u torus#{index} - torus_negative#{index} - cylinder_knockout#{index}'`
 	# `#{mged} 'r torus_shell#{index} u torus#{index} - torus_negative#{index} '`
 end
 # `#{mged} 'r polywell_tori u #{(0...12).map{|index| "torus_shell#{index}"}.join(" u ")}'` 
-puts Dodecahedron.new.class
-dodecahedron = Dodecahedron.new.vertices
 
 
-dodecahedron.each_with_index do |v,index|
+Dodecahedron.vertices.each_with_index do |v,index|
 	`#{mged} 'in a_#{rand} sph #{v[0]} #{v[1]} #{v[2]} 0.2'`  
 end
 
-pentagons = []
-dodecahedron.each_with_index do |v,index|
-	dodecahedron.each_with_index do |v1,index1|
-		dodecahedron.each_with_index do |v2,index2|
-			dodecahedron.each_with_index do |v3,index3|
-				dodecahedron.each_with_index do |v4,index4|
-					if [v,v1,v2,v3,v4].uniq_by {|i|i.hash}.size == [v,v1,v2,v3,v4].size # skip vertice combination with the same vertice twice 
-						a = average(v,v1,v2,v3,v4)
-						# (	`#{mged} 'in a_#{rand} sph #{a[0]} #{a[1]} #{a[2]} 0.07'`  ) if a.r > 1.3
-						pentagons << [v,v1,v2,v3,v4] if a.r > 1.3
-						# puts pentagons.inspect
-						# break
-						
-					end
-					# dodecahedron.row_vectors().each_with_index do |vin,index_in|
-					# 	h = vin - v #vector connecting the point pairs
-					# 	i = v - vin #the inverse of h
-					# 	e = average(v,vin)
-					# `#{mged} 'in a_#{index}_#{index_in} sph #{e[0]} #{e[1]} #{e[2]} 0.06'`  rescue nil
-					# end
-				end
-
-			end
-		end
-	end
-end
-
-
- p =pentagons.uniq_by {|p| p.sort_by{|a|a.hash}.map{|b|b.hash}}
- puts p.inspect
- puts p.size
-q = p.map do |face|
-	# puts "OK#{face.size}"
-	face.map	do |vertex|
-		
-		dodecahedron.rindex(dodecahedron.select { |v| v == vertex   }.first) # find the reverse index
-
-		
-	end
-end
-
-puts q.inspect
+# pentagons = []
+# dodecahedron.each_with_index do |v,index|
+# 	dodecahedron.each_with_index do |v1,index1|
+# 		dodecahedron.each_with_index do |v2,index2|
+# 			dodecahedron.each_with_index do |v3,index3|
+# 				dodecahedron.each_with_index do |v4,index4|
+# 					if [v,v1,v2,v3,v4].uniq_by {|i|i.hash}.size == [v,v1,v2,v3,v4].size # skip vertice combination with the same vertice twice 
+# 						a = average(v,v1,v2,v3,v4)
+# 						# (	`#{mged} 'in a_#{rand} sph #{a[0]} #{a[1]} #{a[2]} 0.07'`  ) if a.r > 1.3
+# 						pentagons << [v,v1,v2,v3,v4] if a.r > 1.3
+# 						# puts pentagons.inspect
+# 						# break
+# 						
+# 					end
+# 					# dodecahedron.row_vectors().each_with_index do |vin,index_in|
+# 					# 	h = vin - v #vector connecting the point pairs
+# 					# 	i = v - vin #the inverse of h
+# 					# 	e = average(v,vin)
+# 					# `#{mged} 'in a_#{index}_#{index_in} sph #{e[0]} #{e[1]} #{e[2]} 0.06'`  rescue nil
+# 					# end
+# 				end
+# 
+# 			end
+# 		end
+# 	end
+# end
+# 
+# 
+#  p =pentagons.uniq_by {|p| p.sort_by{|a|a.hash}.map{|b|b.hash}}
+#  puts p.inspect
+#  puts p.size
+# q = p.map do |face|
+# 	# puts "OK#{face.size}"
+# 	face.map	do |vertex|
+# 		
+# 		dodecahedron.rindex(dodecahedron.select { |v| v == vertex   }.first) # find the reverse index
+# 
+# 		
+# 	end
+# end
+# 
+# puts q.inspect
 
 
  # puts pentagons.inspect
