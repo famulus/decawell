17828d5f11936ff4d8b955ecd4f3a45b06512031
mark <mark@criticalmetrics.com>
2008-11-09 19:20:15 -0500

    fixed a bug where one joint wasnt' rendering

--- mged_controller.rb
+++ mged_controller.rb
@@ -23,23 +23,25 @@ Dodecahedron.edges.each_with_index do |edge,index| #insert the 30 connectors
 	b =cross_product(a,(edge[1]-edge[0])) # this is the vector of the half connector
 	b = b*0.1 # scale the length of the connecter, this is buggy when scale_factor is adjusted
 	`#{mged} 'in connector1_#{index} rcc #{a[0]} #{a[1]} #{a[2]} #{b[0]} #{b[1]} #{b[2]} #{connector_thickness}'` 
+
 	b = Vector[0,0,0]-b# scale the length of the connecter
 	`#{mged} 'in connector2_#{index} rcc #{a[0]} #{a[1]} #{a[2]} #{b[0]} #{b[1]} #{b[2]} #{connector_thickness}'` 
-
 end
+	`#{mged} 'r joints u #{(0..29).map{|index| " connector1_#{index} u connector2_#{index}"}.join(" u ")}'` #combine the pieces
+
 
 
 Dodecahedron.icosahedron.each_with_index do |v,index| # draw the 12 tori
 	v = v*scale_factor
-	`#{mged} 'in torus#{index} tor #{v[0]} #{v[1]} #{v[2]} #{v[0]} #{v[1]} #{v[2]} #{torus_ring_size} #{torus}'`
-	`#{mged} 'in torus_negative#{index} tor #{v[0]} #{v[1]} #{v[2]} #{v[0]} #{v[1]} #{v[2]} #{torus_ring_size} #{torus_negative}'`
-	# `#{mged} 'in cylinder_knockout#{index} rcc #{v[0]} #{v[1]} #{v[2]} #{v[0]} #{v[1]} #{v[2]} #{torus_ring_size* 1.2}.'`
-	# `#{mged} 'r torus_shell#{index} u torus#{index} - torus_negative#{index} - cylinder_knockout#{index}'`
+	`#{mged} 'in torus#{index} tor #{v[0]} #{v[1]} #{v[2]} #{v[0]} #{v[1]} #{v[2]} #{torus_ring_size} #{torus}'` #the torus solid
+	`#{mged} 'in torus_negative#{index} tor #{v[0]} #{v[1]} #{v[2]} #{v[0]} #{v[1]} #{v[2]} #{torus_ring_size} #{torus_negative}'` #this hollow center of the torus
+	# `#{mged} 'in cylinder_knockout#{index} rcc #{v[0]} #{v[1]} #{v[2]} #{v[0]} #{v[1]} #{v[2]} #{torus_ring_size* 1.2}.'` #this removed the face of the torus so we can install coils
+	`#{mged} 'r torus_shell#{index} u torus#{index} u joints - torus_negative#{index} - cylinder_knockout#{index}'` #combine the pieces
 	`#{mged} 'r torus_shell#{index} u torus#{index} - torus_negative#{index} '`
 end
 # `#{mged} 'r polywell_tori u #{(0...12).map{|index| "torus_shell#{index}"}.join(" u ")}'` 
 
-`#{mged} 'r polywell u #{(0...12).map{|index| "torus_shell#{index}"}.join(" u ")} u #{(0...29).map{|index| "connector1_#{index} u connector2_#{index}"}.join(" u ")}'` #union all the parts into a sings polywell object
+`#{mged} 'r polywell u #{(0..11).map{|index| "torus_shell#{index}"}.join(" u ")} u #{(0..29).map{|index| "connector1_#{index} u connector2_#{index}"}.join(" u ")}'` #union all the parts into a sings polywell object
 # `g-stl -o polywell_holow.stl  test3.g polywell`
 
 
