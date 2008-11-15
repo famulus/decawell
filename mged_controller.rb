501738f4ca351d280241a2672b9c83b014b18edd
mark <mark@criticalmetrics.com>
2008-11-15 17:01:32 -0500

    added derived dimentions

--- mged_controller.rb
+++ mged_controller.rb
@@ -7,10 +7,25 @@ require 'facets'
 DB = "test3.g"
 mged ="/usr/brlcad/rel-7.12.2/bin//mged -c  #{DB} "
 scale_factor = 0.75 # global scaling factor
-torus_ring_size = 0.68 *scale_factor
-torus = 0.1 *scale_factor
-torus_negative = 0.90 * torus
-connector_thickness = torus * 0.5
+torus_ring_size = 0.55 *scale_factor
+torus = 0.22 *scale_factor
+torus_negative = 0.85 * torus
+connector_thickness = torus * 0.7
+joint_nudge = 0.90 # this is a percentage scaling of the vector defining the ideal joint location
+
+derived_dimentions = {
+:outside_radius => (Dodecahedron.vertices[0].r) ,
+:torus_midplane_radius => (Dodecahedron.icosahedron[0].r),
+:torus_radius => torus_ring_size,
+:torus_tube_radius => torus,
+:torus_tube_wall_thickness => torus-torus_negative,
+:torus_tube_hollow_radius => torus_negative,
+:joint_radius => connector_thickness,
+}
+
+puts "\n\n"
+derived_dimentions.sort_by{ |k,v| v }.reverse.each { |k,v| puts "#{k}: #{v*100} mm"  }
+puts "\n\n"
 
 `rm ./#{DB}`
 `#{mged} 'units dm'` # set mged's units to decimeter 
@@ -18,7 +33,7 @@ connector_thickness = torus * 0.5
 Dodecahedron.edges.each_with_index do |edge,index| #insert the 30 connectors
 	edge = edge.map{|e|e *scale_factor}
 	a = average(*edge.map{|e|e}) # the ideal location of the connector
-	a = a * 0.95 # nudge the connector closer to the center
+	a = a * joint_nudge # nudge the connector closer to the center
 
 	b =cross_product(a,(edge[1]-edge[0])) # this is the vector of the half connector
 	b = b*0.1 # scale the length of the connecter, this is buggy when scale_factor is adjusted
@@ -42,6 +57,6 @@ end
 # `#{mged} 'r polywell_tori u #{(0...12).map{|index| "torus_shell#{index}"}.join(" u ")}'` 
 
 `#{mged} 'r polywell u #{(0..11).map{|index| "torus_shell#{index}"}.join(" u ")} u #{(0..29).map{|index| "connector1_#{index} u connector2_#{index}"}.join(" u ")}'` #union all the parts into a sings polywell object
-`g-stl -o dodeca_holow.stl  test3.g polywell`
+# `g-stl -o dodeca_holow.stl  test3.g polywell`
 
 
