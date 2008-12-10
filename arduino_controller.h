aebf91e57fc692e005c63c9efe3917ce64a4a1e7
mark <mark@criticalmetrics.com>
2008-12-10 13:38:09 -0500

    cleanup

--- arduino_controller.h
+++ arduino_controller.h
@@ -1,4 +1,5 @@
-int outPins[] = { 6,7,8,12,13};
+int outPins[] = { 
+  6,7,8,12,13};
 int val;
 int incomingByte = 0;	// for incoming serial data
 
@@ -8,59 +9,62 @@ void setup() {
   for(int i = 0; i < 5; i++ ) {
     pinMode(outPins[i], OUTPUT);
   }
-  
-        digitalWrite(8,LOW); // turn motors off by default
+
+  digitalWrite(8,LOW); // turn motors off by default
 }
 
 
-      
+
 void loop() {
   if (Serial.available() > 0) {
     // read the incoming byte:
     incomingByte = Serial.read();
-incomingByte = incomingByte - '0'; 
+    incomingByte = incomingByte - '0'; 
 
 
-// global power off/on
-if(incomingByte == 0){
+    // global power off/on
+    if(incomingByte == 0){
       digitalWrite(8,LOW);
-}
-if(incomingByte == 1){
+    }
+    if(incomingByte == 1){
       digitalWrite(8,HIGH);
-}
+    }
 
 
-//winder stepper motor
+    //winder stepper motor
 
-if(incomingByte == 2){
+    if(incomingByte == 2){
       digitalWrite(6,HIGH);
-         delay(2);
+      delay(2);
       digitalWrite(6,LOW);
-}
-if(incomingByte == 3){
+      delay(2);
+    }
+    if(incomingByte == 3){
       digitalWrite(7,HIGH);
-}
-if(incomingByte ==4){
+    }
+    if(incomingByte ==4){
       digitalWrite(7,LOW);
-}
+    }
 
 
 
-//wire guide stepper motor
+    //wire guide stepper motor
 
-if(incomingByte == 5){
+    if(incomingByte == 5){
       digitalWrite(12,HIGH);
-         delay(2);
+      delay(2);
       digitalWrite(12,LOW);
-}
-if(incomingByte == 6){
+      delay(2);
+    }
+    if(incomingByte == 6){
       digitalWrite(13,HIGH);
-}
-if(incomingByte == 7){
+    }
+    if(incomingByte == 7){
       digitalWrite(13,LOW);
+    }
+
+
+
+
+  }
 }
-
-
-
-
-}}
