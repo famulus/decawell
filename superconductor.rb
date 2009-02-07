b635cf0dd8775a717af36b6ce656bd8cbe9e595f
mark <mark@criticalmetrics.com>
2009-02-06 21:50:40 -0500

    base force off of one loop

--- superconductor.rb
+++ superconductor.rb
@@ -22,18 +22,18 @@ ybco_width = Unit("4 mm") >> Unit("cm")
 
 puts ybco_area = (torus_circumference>>Unit("cm")) * ybco_width
 
-puts engineering_current_density =  Unit("21 kA/cm^2") >> Unit("ampere/cm^2")
+puts engineering_current_density =  Unit("21 kA/cm^2") >> Unit("ampere/cm**2")
 
 puts current_density = ybco_area * engineering_current_density
 
 torus_midplane_radius = 79.4172368111867 
 # Ampère's force law calculations  http://en.wikipedia.org/wiki/Ampère%27s_force_law
 puts "magnetic_constant"
-magnetic_constant = (4*Math::PI * (10.0**-7)) * Unit("newton/ampere**2")
+puts magnetic_constant = (4*Math::PI * (10.0**-7)) * Unit("newton/ampere**2")
 puts magnetic_force_constant = magnetic_constant / (2*Math::PI)
 puts seperation_of_wires = (torus_midplane_radius*mm) >> Unit("m") # in m
 puts coil_force_per_meter = magnetic_force_constant * ((current_density**2)/seperation_of_wires)
-puts coil_force = coil_force_per_meter * (ybco_length >> Unit('m'))
+puts coil_force = coil_force_per_meter * (torus_circumference >> Unit('m'))
 puts  ((coil_force_per_meter * (ybco_length >> Unit('m')))/9.8).scalar # Kg of force
 puts  ((coil_force_per_meter * (ybco_length >> Unit('m')))/9.8).scalar / 816 # number of honda civics of force
 puts (turns )*current_density
