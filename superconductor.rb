617da764787c85966e4cd21a2b302c12316eaf7c
mark <mark@criticalmetrics.com>
2009-02-11 11:53:55 -0500

    fix critical current calcs for superconductor tape

--- superconductor.rb
+++ superconductor.rb
@@ -35,6 +35,4 @@ puts magnetic_force_constant = magnetic_constant / (2*Math::PI)
 puts seperation_of_wires = (torus_midplane_radius*mm) >> Unit("m") # in m
 puts coil_force_per_meter = magnetic_force_constant * ((current_density**2)/seperation_of_wires)
 puts coil_force = coil_force_per_meter * (torus_circumference >> Unit('m'))
-puts  ((coil_force_per_meter * (torus_circumference >> Unit('m')))/9.8).scalar # Kg of force
-puts  ((coil_force_per_meter * (torus_circumference >> Unit('m')))/9.8).scalar / 816 # number of honda civics of force
-puts (turns )*current_density
+puts  ((coil_force_per_meter * (torus_circumference >> Unit('m')))/9.8).scalar*1000 # grams of force
