4fe9b713c54c2d0b7b91561a6ed862cdd628eab1
mark <mark@criticalmetrics.com>
2009-02-22 00:39:46 -0500

    added B-fleid calculation

--- superconductor.rb
+++ superconductor.rb
@@ -5,9 +5,9 @@ require 'ruby-units'
 # require 'winder'
 
 class Unit < Numeric
-  # @@USER_DEFINITIONS =
-  #   {'<tesla>'=>[%w{tesla T}, 1, :magnetism, %w{<weber>},%w{<meter> <meter>} ]}
-  # Unit.setup
+  @@USER_DEFINITIONS =
+    {'<eV>'=>[%w{eV electron_volt electronvolt},  1.60217653*(10.0**-19), :energy, %w{J} ]}
+  Unit.setup
 end
 
 mm = Unit("mm")
@@ -15,8 +15,9 @@ amp = Unit("amp")
 ohm = Unit("ohm")
 
 ic = Unit("100 ampere") # the critical current of the superconducting cable
-
+eo = Unit("1 electronvolt")
 torus_radius = 34.62 * Unit("mm")
+torus_midplane_radius = 79.4172368111867 * Unit("mm")
 
 puts torus_circumference = torus_radius *2* Math::PI
 
@@ -32,18 +33,24 @@ puts ybco_area = (torus_circumference>>Unit("cm")) * ybco_width
 puts engineering_current_density =  Unit("21 kA/cm^2") >> Unit("ampere/cm**2")
 
 
-torus_midplane_radius = 79.4172368111867 
+
 # Ampère's force law calculations  http://en.wikipedia.org/wiki/Ampère%27s_force_law
 puts "magnetic_constant"
 puts magnetic_constant = (4*Math::PI * (10.0**-7)) * Unit("newton/ampere**2")
 puts magnetic_force_constant = magnetic_constant / (2*Math::PI)
-puts seperation_of_wires = (torus_midplane_radius*mm) >> Unit("m") # in m
+puts seperation_of_wires = (torus_midplane_radius) >> Unit("m") # in m
 puts coil_force_per_meter = magnetic_force_constant * ((ic**2)/seperation_of_wires)
 puts coil_force = coil_force_per_meter * ((torus_circumference*turns) >> Unit('m'))
 puts  ((coil_force_per_meter * ((torus_circumference*turns) >> Unit('m')))/9.8).scalar*1000 # grams of force
 
 u = 2*Math::PI * (10.0**-7) * ((Unit("tesla") * Unit("m"))/Unit("ampere"))
 puts b_field =  u*(ic*turns)/(torus_radius >> Unit("meter"))
+puts b_field >> Unit('G')
+puts gwb = (((b_field >> Unit('G')) * (torus_midplane_radius >> Unit('cm')))**2) / 110*eo
+
+
+
+
 # bb=2*Math.PI*Math.pow(10,-7)*ii/rr;
 
 
