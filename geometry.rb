59be6ee446becde2573ddcc2d9ea3e44e3ebcbdc
mark <mark@criticalmetrics.com>
2009-04-01 17:57:56 -0400

    changes we want to keep in geometry.rb

--- geometry.rb
+++ geometry.rb
@@ -18,6 +18,16 @@ class Vector
 		Vector[0,0,0] - self
 	end
 	
+	def self.average(*args)
+		v = args.first
+		args.each_with_index do |vector,index|
+			v = v+vector unless index == 0
+
+		end
+		v*(1/(args.size).to_f)
+	end
+
+	
 	
 end
 
@@ -69,20 +79,36 @@ module Geometry
 				].row_vectors()
 		end
 
+		def self.faces_indices
+			[[0, 1, 12, 16, 17], [0, 3, 8, 10, 16], [0, 5, 8, 12, 14], [1, 2, 9, 11, 17], [1, 6, 9, 12, 14], [2, 3, 13, 16, 17], [2, 4, 11, 13, 15], [3, 7, 10, 13, 15], [4, 6, 9, 11, 19], [4, 7, 15, 18, 19], [5, 6, 14, 18, 19], [5, 7, 8, 10, 18]]
+		end
+		def self.edge_indices 
+			[[18, 19], [16, 17], [13, 15], [12, 14], [9, 11], [8, 10], [0, 16], [0, 8], [7, 15], [7, 10], [6, 19], [0, 12], [6, 14], [6, 9], [5, 18], [7, 18], [5, 14], [5, 8], [4, 19], [4, 15], [4, 11], [3, 16], [3, 13], [3, 10], [2, 17], [2, 13], [1, 9], [2, 11], [1, 17], [1, 12]]
+		end
+
+		def self.faces_for_edge_indices
+			self.edge_indices.map do |edge|
+				self.faces_indices.select{|face| face.include?(edge[0]) && face.include?(edge[1]) }
+
+			end
+		end
+
+		def self.faces_for_edge
+			self.faces_for_edge_indices.map { |a|a.map { |b| b.map { |c| self.vertices[c]  }}  }
+		end
+		
 		def self.faces
-			face_indexes = [[0, 1, 12, 16, 17], [0, 3, 8, 10, 16], [0, 5, 8, 12, 14], [1, 2, 9, 11, 17], [1, 6, 9, 12, 14], [2, 3, 13, 16, 17], [2, 4, 11, 13, 15], [3, 7, 10, 13, 15], [4, 6, 9, 11, 19], [4, 7, 15, 18, 19], [5, 6, 14, 18, 19], [5, 7, 8, 10, 18]]
-			face_indexes.map { |f| f.map{|v|vertices[v]}  }
+			self.faces_indices.map { |f| f.map{|v|vertices[v]}  }
 		end
 
 		def self.icosahedron
 			faces.map do |face|
-				midpoint  = average(*face.map { |v|v  })
+				midpoint  = Vector.average(*face.map { |v|v  })
 			end
 		end
 
 		def self.edges
-			edges =[[18, 19], [16, 17], [13, 15], [12, 14], [9, 11], [8, 10], [0, 16], [0, 8], [7, 15], [7, 10], [6, 19], [0, 12], [6, 14], [6, 9], [5, 18], [7, 18], [5, 14], [5, 8], [4, 19], [4, 15], [4, 11], [3, 16], [3, 13], [3, 10], [2, 17], [2, 13], [1, 9], [2, 11], [1, 17], [1, 12]]
-			edges.map { |f| f.map{|v|vertices[v]} }
+			self.edge_indices.map { |f| f.map{|v|vertices[v]} }
 		end
 
 	end
