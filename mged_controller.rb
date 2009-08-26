e0176c8141ec51ce8b8c95b56a61d661aa168a86
mark <mark@criticalmetrics.com>
2009-08-26 13:28:16 -0400

    back to high rez for STL

--- mged_controller.rb
+++ mged_controller.rb
@@ -56,8 +56,8 @@ coil = Coil.new((torus_negative*2), coil_wire_diameter, torus_ring_size)
 
 channel_thickness = (ribbon_thickness*turns)+1
 
- # tolerance_distance = 0.01
-tolerance_distance = 0.08
+ tolerance_distance = 0.01
+# tolerance_distance = 0.08
 
 #Joule heating calculations
 drive_amps = 2000.0 * amp
