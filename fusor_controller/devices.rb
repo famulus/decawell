bec053a49e3cc45ed51c921045c7c6de2f9a4719
temp <temp@temps-power-mac-g4-agp-graphics.local>
2009-11-17 16:24:06 -0500

    added code to convert voltages from devices to actual reading

--- fusor_controller/devices.rb
+++ fusor_controller/devices.rb
@@ -4,3 +4,10 @@ class Hornet
     pressure_in_millitorr = pressure_in_torr*1000
   end
 end
+
+class Glassman
+  def self.convert_voltage(voltage)
+    pressure_in_torr = 10**(voltage - 10)
+    pressure_in_millitorr = pressure_in_torr*1000
+  end
+end
