a39188a0b4452527a89b7e15f9781f2cbac8831a
famulus <famulus.fusion@gmail.com>
2011-08-09 10:56:23 -0400

    good start to ceramic copper coil

--- mged_controller.rb
+++ mged_controller.rb
@@ -9,8 +9,8 @@ def chassis # the chassis is the inner section of the magrid
 
 	Cube.octahedron.each_with_index do |v,index| # draw the 6 tori
 		v = v*@scale_factor		
-		`#{@mged} 'in torus#{index} tor #{v.mged} #{v.mged} #{@torus_ring_size} #{@torus} '` #the eto solid
-		`#{@mged} 'in torus_negative#{index} tor #{v.mged} #{v.mged} #{@torus_ring_size+@torus} #{@torus*0.7} '` #the eto solid
+		`#{@mged} 'in torus#{index} tor #{v.mged} #{v.mged} #{@torus_ring_size} #{@torus} '` #the torus solid
+		`#{@mged} 'in torus_negative#{index} tor #{v.mged} #{v.mged} #{@torus_ring_size+@torus} #{@torus*0.7} '` #the depression negative to hold the coils
 
 	end
 	Cube.edges.each_with_index do |edge,index| #insert the  joints
@@ -19,13 +19,19 @@ def chassis # the chassis is the inner section of the magrid
 		a = a * @joint_nudge # nudge the joint closer to the center
 		b = cross_product(a,(edge[1]-edge[0])) # this is the vector of the half joint
 		b = b.normal*@scale_factor* @joint_nudge_length # get the unit vector for this direction and scale
+		
 	end
-			`#{@mged} 'in electron_gun_sphere sph #{(Cube.octahedron.first*@scale_factor*1.5).mged} 17'` #sphere to hold electron gun
+	
+	base_vector = Cube.octahedron.first
+	
+	`#{@mged} 'in electron_gun_sphere sph #{(base_vector*@scale_factor*1.5).mged} 17'` #sphere to hold electron gun
+	`#{@mged} 'in electron_gun_hollow rcc #{(base_vector*@scale_factor*1.5).mged} #{base_vector*18} 8'` #hollow to fit copper tube
+	`#{@mged} 'in electron_gun_hollow_inverse rcc #{(base_vector*@scale_factor*1.5).mged} #{base_vector.inverse*18} 8'` #hollow to fit copper tube
 
 	
 	
 	`#{@mged} 'comb solid.c u #{(0..5).map{|index| "torus#{index}"}.join(" u ")} u electron_gun_sphere'` #combine the pieces
-	`#{@mged} 'comb negative_form.c u #{(0..5).map{|index| "torus_negative#{index}"  }.join(' u ')}'` #combine the pieces
+	`#{@mged} 'comb negative_form.c u electron_gun_hollow u electron_gun_hollow_inverse u #{(0..5).map{|index| "torus_negative#{index}"  }.join(' u ')}'` #combine the pieces
 	`#{@mged} 'r chassis u solid.c - negative_form.c'` #remove negative from positive
 end
 
@@ -73,62 +79,9 @@ max_torus = (a-b).r
 
 @torus_ring_size = max_torus/1.305 #0.700 *@scale_factor # the main torus shape
 @torus = 0.14 *@scale_factor 
-@torus_negative = 0.72 * @torus 
-@joint_radius = (@ribbon_width/2) + (@minimum_wall_thickness)
-@joint_negative_radius = (@ribbon_width/2) + 0.2
 @joint_nudge = 0.84 # this is a percentage scaling of the vector defining the ideal joint location
 @joint_nudge_length = 0.16
-@coil_wire_diameter = 1.1  # mm test wire
-@channel_thickness = (@ribbon_thickness*@turns)+1
-@tolerance_distance = 0.01
-@drive_amps = 80
 
-
-@electron_gun_distance = @torus_midplane_radius *1.5 
-
-
-
-# Ampère's force law calculations  http://en.wikipedia.org/wiki/Ampère%27s_force_law
-magnetic_constant = (4*Math::PI * (10.0**(-7))) * Unit("newton/ampere**2")
-magnetic_force_constant = magnetic_constant / (2*Math::PI)
-seperation_of_wires = (@torus_midplane_radius*mm) >> Unit("m") # in m
-coil_force_per_meter = magnetic_force_constant * ((@drive_amps**2)/seperation_of_wires)
-
-
-
-
-derived_dimentions = {
-	:outside_radius => @outside_radius,
-	:torus_midplane_radius => @torus_midplane_radius,
-	:torus_radius => @torus_ring_size,
-	:torus_tube_radius => @torus,
-	:torus_tube_wall_thickness => @torus-@torus_negative,
-	:torus_tube_hollow_radius => @torus_negative,
-	:joint_radius => @joint_radius,
-	:joint_negative_radius => @joint_negative_radius,
-	:donut_exterier_radius => @torus_ring_size +@torus ,
-	:donut_hole_radius => @torus_ring_size -@torus,
-}
-
-
-amperes_force = {
-	:magnetic_constant => magnetic_constant, 
-	:magnetic_force_constant => magnetic_force_constant, 
-	:seperation_of_wires => seperation_of_wires, 
-	:coil_force_per_meter => coil_force_per_meter, 
-}
-
-
-
-# Print the dimentions and properties of the current magrid to standard output.
-puts "\n\n"
-derived_dimentions.select{|k,v| v.class != Unit}.sort_by{|k,v| v}.reverse.each { |k,v| puts "#{k}: #{v} mm"  }
-puts "\n\n"
-[amperes_force].each do |topic|
-	puts "\n\n"
-	topic.select{|k,v| v.class == Unit}.each { |k,v| puts "#{k}: #{v}"  }
-	puts "\n\n"
-end
 
 
 `#{@mged} 'units mm'` # set mged's units to millimeter 
@@ -136,7 +89,6 @@ end
 
 `rm -f ./temp/*` #clear out temp files
 chassis # generate the parts in mged
-# lid 
 
 # this block prepares a snapshot picture of the part
 def render_view(ae ="ae 135 -35 180", zoom = 0.3 )
